918da787b4f3fca01998dca2623d091eadf1472d
Leo Torres <leotorres114@gmail.com>
2024-02-14 19:01:17 -0600

    Update manual data fix script
    updated variable names

--- src/manual_data_fixes.R
+++ src/manual_data_fixes.R
@@ -3,17 +3,17 @@
 # This code manually adds the monthly average for Jan-Oct 2023 to
 # November and December 2023.
 
-rideon_vrh_nov2023 <- rev_hrs %>%
+rideon_vrh_2023_avg <- rev_hrs %>%
   filter(ntdid == 30051 & year == 2023 & month != 11 & month != 12) %>%
   pull(vrh) %>%
   mean()
 
 rev_hrs$vrh[rev_hrs$ntdid == 30051 &
               rev_hrs$month == 11 & 
-              rev_hrs$year == 2023] <- rideon_vrh_nov2023
+              rev_hrs$year == 2023] <- rideon_vrh_2023_avg
 
 rev_hrs$vrh[rev_hrs$ntdid == 30051 &
               rev_hrs$month == 12 & 
-              rev_hrs$year == 2023] <- rideon_vrh_nov2023
+              rev_hrs$year == 2023] <- rideon_vrh_2023_avg
 
 ####################################################################################################
